b599cd281c76c1cdda5fbf20bbe776df9f0347df
aHermitte <ahermitte@enseirb-matmeca.fr>
2024-12-18 14:37:12 +0100

    feat: implementation for composed & simple trip

--- src/main/java/com/testlog/projet/ComposedTrip.java
+++ src/main/java/com/testlog/projet/ComposedTrip.java
@@ -1,4 +1,44 @@
 package com.testlog.projet;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ComposedTrip {
+  private List<SimpleTrip> trips;
+
+  public ComposedTrip(List<SimpleTrip> trips) {
+    this.trips = trips;
+  }
+
+  public String getDeparture_city() {
+    return trips.get(0).getDeparture_city();
+  }
+
+  public String getArrival_city() {
+    return trips.get(trips.size() - 1).getArrival_city();
+  }
+
+  public List<TransportationMode> getMode() {
+    List<TransportationMode> modes = new ArrayList<>();
+    for (SimpleTrip trip : trips) {
+      modes.add(trip.getMode());
+    }
+    return modes;
+  }
+
+  public double getPrice() {
+    double price = 0;
+    for (SimpleTrip trip : trips) {
+      price += trip.getPrice();
+    }
+    return price;
+  }
+
+  public long getDeparture_time() {
+    return trips.get(0).getDeparture_time().getEpochSecond();
+  }
+
+  public long getArrival_time() {
+    return trips.get(trips.size() - 1).getArrival_time().getEpochSecond();
+  }
 }
--- src/main/java/com/testlog/projet/SimpleTrip.java
+++ src/main/java/com/testlog/projet/SimpleTrip.java
@@ -1,4 +1,71 @@
 package com.testlog.projet;
 
+import java.time.Instant;
+
 public class SimpleTrip {
+  private String departure_city;
+  private String arrival_city;
+  private TransportationMode mode;
+  private double price;
+  private Instant departure_time;
+  private Instant arrival_time;
+
+  public SimpleTrip(String departure_city, String arrival_city, TransportationMode mode, double price,
+      Instant departure_time, Instant arrival_time) {
+    this.departure_city = departure_city;
+    this.arrival_city = arrival_city;
+    this.mode = mode;
+    this.price = price;
+    this.departure_time = departure_time;
+    this.arrival_time = arrival_time;
+  }
+
+  public String getDeparture_city() {
+    return departure_city;
+  }
+
+  public void setDeparture_city(String departure_city) {
+    this.departure_city = departure_city;
+  }
+
+  public String getArrival_city() {
+    return arrival_city;
+  }
+
+  public void setArrival_city(String arrival_city) {
+    this.arrival_city = arrival_city;
+  }
+
+  public TransportationMode getMode() {
+    return mode;
+  }
+
+  public void setMode(TransportationMode mode) {
+    this.mode = mode;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  public void setPrice(double price) {
+    this.price = price;
+  }
+
+  public Instant getDeparture_time() {
+    return departure_time;
+  }
+
+  public void setDeparture_time(Instant departure_time) {
+    this.departure_time = departure_time;
+  }
+
+  public Instant getArrival_time() {
+    return arrival_time;
+  }
+
+  public void setArrival_time(Instant arrival_time) {
+    this.arrival_time = arrival_time;
+  }
+
 }
