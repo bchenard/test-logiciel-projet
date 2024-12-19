853645fed4ddd9870afe17f1b3ed21d4a2a78964
Arthur Le Floch <arthur.le_floch@bordeaux-inp.fr>
2024-12-19 11:12:18 +0100

    Add city optimizer method

--- src/main/java/com/testlog/projet/optimize/CityOptimizer.java
+++ src/main/java/com/testlog/projet/optimize/CityOptimizer.java
@@ -1,4 +1,16 @@
 package com.testlog.projet.optimize;
 
-public class CityOptimizer implements ICityOptimizer{
+import com.testlog.projet.criteria.HotelCriteria;
+import com.testlog.projet.types.Activity;
+import com.testlog.projet.types.Hotel;
+import com.testlog.projet.types.Pair;
+
+import java.util.List;
+
+public class CityOptimizer implements ICityOptimizer {
+
+    @Override
+    public Pair<Hotel, List<Activity>> optimize(HotelCriteria hotelCriteria, ActivityCriteria activityCriteria, String city, int days, int budget) {
+        return null;
+    }
 }
---
+++ src/main/java/com/testlog/projet/types/Pair.java
@@ -0,0 +1,4 @@
+package com.testlog.projet.types;
+
+public record Pair<U, V>(U first, V second) {
+}
