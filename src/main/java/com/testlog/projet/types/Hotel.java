3b003f0d2e8ba8ef90301b1bf0e62e01b0221d29
killianmannarelli <kmannarelli@ensc.fr>
2025-01-08 10:02:07 +0100

    Fix : HotelServiceTest.java

--- src/main/java/com/testlog/projet/types/Hotel.java
+++ src/main/java/com/testlog/projet/types/Hotel.java
@@ -1,4 +1,4 @@
 package com.testlog.projet.types;
 
-public record Hotel(String city, LatLng coordinates, int stars, String name, double price) {
+public record Hotel(String city,LatLng coordinates, int stars, String name, double price, String address) {
 }
